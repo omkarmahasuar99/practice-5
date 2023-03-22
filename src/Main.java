1dcd9309b5307cf8b86888d370d2650910eb4f2f
omkarmahasuar99 <126793930+omkarmahasuar99@users.noreply.github.com>
2023-03-22 14:40:49 +0530

    true if both positive

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        int x=9;
+        int y=-89;
+        if(x>=0 && y>=0)
+            System.out.println("true");
+        else System.out.println("false");
+    }
+}
